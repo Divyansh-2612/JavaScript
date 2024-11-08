2de0b8269469ae673554647fa5310e465513b87e
Divyansh-2612 <155242293+Divyansh-2612@users.noreply.github.com>
2024-11-08 18:37:40 +0000

    variables

---
+++ JavaScript_chai_aur_code/1Variable.js
@@ -0,0 +1,14 @@
+const id=1234
+let name="Divyansh"
+var age=22
+city="Bhopal"
+name="shivansh"
+age=19
+city="Bpl"
+console.table([id,name,age,city])
+for(var i=0;i<10;i++){
+    setTimeout(()=> console.log(i),0)
+}
+let a=2
+let val=Boolean(a)
+console.log(val)
