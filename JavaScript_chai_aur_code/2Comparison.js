553998223bde4cc055ab761da5858e582d86f982
Divyansh-2612 <155242293+Divyansh-2612@users.noreply.github.com>
2024-11-10 05:04:22 +0000

    comparison

---
+++ JavaScript_chai_aur_code/2Comparison.js
@@ -0,0 +1,14 @@
+let a=undefined;
+let b=null;
+console.table([Number(a),Number(b)]);
+console.log(b>0)
+console.log(b==0)
+console.log(b>=0)
+console.log(a>0)
+console.log(a<0)
+console.log(a==0)
+let bignumber=12121233n
+// n represent bignumber
+let c=Symbol('123')
+let d=Symbol('123')
+console.log(c==d)
